d7d6ef006e45465e7a53cfc5806adce79e3e18ab
Ross Condie <rosscondie@gmail.com>
2023-05-02 11:17:49 +0200

    imported Provider and wrapped the app in Provider component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Uber Capstone Project</Text>
+      </View>
+    </Provider>
   );
 }
 // in react native flexbox defaults to a column
