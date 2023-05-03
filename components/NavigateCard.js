96076321c7df2b1ad9ab2bcd3f96a508e6ec1acd
Ross Condie <rosscondie@gmail.com>
2023-05-03 13:58:53 +0200

    added NavigateCard and RideOptions component

---
+++ components/NavigateCard.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+
+const NavigateCard = () => {
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text>NavigateCard</Text>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
+
+const styles = StyleSheet.create({});
