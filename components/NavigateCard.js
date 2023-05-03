05cc9dc68baf6c1f29c886092f5589b493a016a4
Ross Condie <rosscondie@gmail.com>
2023-05-03 14:12:53 +0200

    added input field with GooglePlaces Autocomplete to NavigateCard

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
 
 const NavigateCard = () => {
   return (
@@ -8,10 +10,35 @@ const NavigateCard = () => {
       <Text style={tw`text-center py-5 text-xl font-semibold`}>
         Good Morning CodeClan
       </Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={inputBoxStyles}
+            placeholder="Where to?"
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={300}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({});
+const inputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 15,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: '#DDDDDF',
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
