af3456efa93cfc9d231c110a6ceea3e71db5fe22
Ross Condie <rosscondie@gmail.com>
2023-05-03 14:39:54 +0200

    added navigation when destination is selected to show RideOptionsCard

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,8 +3,13 @@ import React from 'react';
 import tw from 'twrnc';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/native';
 
 const NavigateCard = () => {
+  const dispatch = useDispatch(); // useDispatch is a hook
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl font-semibold`}>
@@ -13,7 +18,24 @@ const NavigateCard = () => {
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
           <GooglePlacesAutocomplete
-            styles={inputBoxStyles}
+            styles={inputBoxStyles} // can't use tailwind because we need to override default styles in component
+            fetchDetails={true}
+            returnKeyType={'search'}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate('RideOptionsCard');
+            }}
+            enablePoweredByContainer={false} // this gets rid of the google logo
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: 'en',
+            }}
             placeholder="Where to?"
             nearbyPlacesAPI="GooglePlacesSearch"
             debounce={300}
