eecb0cedec7d8449f0305aa24cc9bbee9b21a5ce
nicolasgoetschel <nicgoe@hotmail.com>
2023-05-03 15:50:39 +0100

    NavFavourites component working

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const data = [
+  {
+    id: '123',
+    icon: 'home',
+    location: 'Home',
+    destination: '37 Castle Terrace, Edinburgh EH1 2EL',
+  },
+  {
+    id: '456',
+    icon: 'briefcase',
+    location: 'Work',
+    destination: 'Clockwise, 77, Renfrew St, Glasgow G2 3BZ',
+  },
+];
 
 const NavFavourites = () => {
   return (
-    <View>
-      <Text>NavFavourites</Text>
-    </View>
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <Text>Test</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
