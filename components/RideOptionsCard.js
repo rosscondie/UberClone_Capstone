6e190137bc8a7b76f48f1edb5c3536a4dc5f044b
nicolasgoetschel <nicgoe@hotmail.com>
2023-05-04 09:26:27 +0100

    button working

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -3,12 +3,18 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'twrnc';
 import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
-        <TouchableOpacity style={tw`absolute top-3 left-5 p-3 rounded-full`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('NavigateCard')}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>Select a ride</Text>
