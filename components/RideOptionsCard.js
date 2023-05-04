506688c95f95cbc52a695cd9ae90f135b0b182b5
nicolasgoetschel <nicgoe@hotmail.com>
2023-05-04 09:18:49 +0100

    Select a ride and icon added

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
+import { Icon } from 'react-native-elements';
 
 const RideOptionsCard = () => {
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity>
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a ride</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
